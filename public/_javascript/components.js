57c1a2415c3f87f8aa504f17e3eeb253ac4d626a
yrllanbrandao <yrllanbrandao@outlook.com>
2023-04-12 04:14:46 -0300

    style: change backgrounds  url to local

--- public/_javascript/components.js
+++ public/_javascript/components.js
@@ -5,25 +5,6 @@ import { search } from "./components/search.js";
 import { getOriginalHeight } from "./components/getOriginalHeight.js"
 import { preload } from "./preload.js";
 
-const links = document.querySelectorAll("a");
-
-links.forEach(link =>{
-    link.addEventListener("click", ()=>{
-
-        if ($("#progress").length === 0) {
-            // inject the bar..
-            $("body").append($("<div><b></b><i></i></div>").attr("id", "progress"));
-            
-            // animate the progress..
-            $("#progress").width("101%").delay(800).fadeOut(1000, function() {
-              // ..then remove it.
-              $(this).remove();
-            });  
-          }
-    })
-   
-})
-
 dropdownFunction();
 menuToggle();
 tooltip();
--- public/_javascript/preload.js
+++ public/_javascript/preload.js
@@ -1,14 +1,7 @@
 export async function preload() {
   const preloading = document.querySelector(".preloading");
-  const url = await getComputedStyle(document.documentElement)
-    .getPropertyValue("--bg-url")
-    .replaceAll('"', "");
-
-  const img = document.createElement("img");
-
-  img.src = url;
-
-  img.addEventListener("load", () => {
+ 
+  window.addEventListener("load", () => {
     setTimeout(() => {
       preloading.classList.add("disabled");
     }, 300);
